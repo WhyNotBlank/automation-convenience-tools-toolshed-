ef3dc94d2da73c9b169c7918da354d770e06ca18
qoibiman <frastoclackto@gmail.com>
2025-12-01 23:51:33 +0100

    deprecated autoClicker.py file
    This will no longer work at all.

---
+++ autoClicker(deprecated).py
@@ -0,0 +1,2 @@
+print("The origenal file has been deleted, you can find an exact replica in some folder in this repository.")
+#The origenal file has been deleted, you can find an exact replica in some folder in this repository.
