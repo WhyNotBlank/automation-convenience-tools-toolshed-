7bc3cb4e93249d86fedeacae874a312e05cb8822
qoibiman <frastoclackto@gmail.com>
2025-12-07 17:53:04 +0100

    first C program

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdbool.h>
+
+int main(){
+
+//ninjas be runnin like teenage mutants    
+
+/*
+
+wsp
+ciao
+
+*/
+
+printf("cow\n");
+printf("dick\n");
+
+Sleep(100000);
+
+return 0;
+
+}
